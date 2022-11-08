08f629283f01a788ee783daaecbe6274596b0703
EricKisendi <erickisendi18@gmail.com>
2022-11-08 14:32:04 +0300

    my card

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,8 @@ import './Footer.css';
 export default function Foot(){
     return (
         <div className="Bigfoot">
-            <i className="material-icons">facebook</i>
-            <i className="material-icons">snapchat</i>
+            <i className="material-icons fb">facebook</i>
+            <i className="material-icons snap">snapchat</i>
             <i><box-icon type='logo' color = "aqua" name='twitter'></box-icon></i>
             <box-icon type='logo' color="#FF8787" name='pinterest'></box-icon>
         </div>
